84ff3bca7e573d311581d3774734c606320ac92e
Sid Patel <51685505+Syzygy05@users.noreply.github.com>
2020-02-21 13:32:09 -0500

    Update backend.php
    modified sql query and if statement verification simplification

--- backend.php
+++ backend.php
@@ -11,36 +11,21 @@
 	$data = json_decode($v);
 
 	// User ID and Hashed Password from Middle End
-	$user=$data->{'name'};
 	$user=$data->{'user'};
 	$pw_hashed = $data->{'password'};
 
 	//No need for hardcoded user here - have that in the database directly, and just type that into frontend like any credential
 
 	// SQL Query 
-	$sql = "SELECT USER, PASS FROM USERLOG WHERE USER='$user'";
-	$sql = "SELECT USER, PASS FROM USERLOG WHERE USER='$user' AND PASSWORD='$pw_hashed'";
+	$sql = "SELECT * FROM USERLOG WHERE USER='$user' AND PASSWORD='$pw_hashed'";
 	// SQL Query Results
 	$result = $conn->query($sql);
 
 	// Check if result exists
-	if ($result->num_rows > 0) {
-
-		$row = $result->fetch_assoc();
-
-		$u = $row["USER"];
-		$hp = $row["PASS"];
-
-		if ($pw_hashed == $hp) {
-			$login_status = "success";
-		}
-	}
-	else {
 	if ($result->num_rows > 0)
 		$login_status = "success";
 	else
 		$login_status = "failure";
-	}
 
 	mysqli_close($conn); //close connection before returning from script
 
